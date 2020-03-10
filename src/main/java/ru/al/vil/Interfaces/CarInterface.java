77c280731a8cdf77704765c3df274016a2ecbed4
sanya13091984 <61901299+sanya13091984@users.noreply.github.com>
2020-03-10 12:52:39 +0300

    //2

--- src/main/java/ru/al/vil/Interfaces/CarInterface.java
+++ src/main/java/ru/al/vil/Interfaces/CarInterface.java
@@ -2,7 +2,7 @@ package ru.al.vil.Interfaces;
 
 public interface CarInterface {
 
-        String get(String x, String y);
+        String get(String x, String y);//2
         Integer put( int x, int y);
         String post(String x, String y);
         String del();
